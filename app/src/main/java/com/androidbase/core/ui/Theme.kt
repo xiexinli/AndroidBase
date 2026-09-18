48a429f1367d860f4afaa9f0cc4a3cdae7aac4ee
xiexinli <xiexinli@dhgate.com>
2026-09-18 16:50:01 +0800

    搭建开发基础能力。

--- app/src/main/java/com/androidbase/core/ui/Theme.kt
+++ app/src/main/java/com/androidbase/core/ui/Theme.kt
@@ -5,5 +5,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val Colors = lightColorScheme(primary = Color(0xFF3859E9), secondary = Color(0xFF5F5B71), tertiary = Color(0xFF006C4C))
-@Composable fun AndroidBaseTheme(content: @Composable () -> Unit) { MaterialTheme(colorScheme = Colors, content = content) }
+private val Colors = lightColorScheme(
+    primary = Color(0xFF3859E9),
+    secondary = Color(0xFF5F5B71),
+    tertiary = Color(0xFF006C4C)
+)
+
+@Composable
+fun AndroidBaseTheme(content: @Composable () -> Unit) {
+    MaterialTheme(colorScheme = Colors, content = content)
+}
